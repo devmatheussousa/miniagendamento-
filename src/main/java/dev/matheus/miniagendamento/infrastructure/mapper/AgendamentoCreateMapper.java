0c512924353d211f77707fd30924c765f06e4682
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-19 18:33:46 -0300

    feat: cria o mapper agendamento

---
+++ src/main/java/dev/matheus/miniagendamento/infrastructure/mapper/AgendamentoCreateMapper.java
@@ -0,0 +1,40 @@
+package dev.matheus.miniagendamento.infrastructure.mapper;
+
+import dev.matheus.miniagendamento.core.entities.Agendamento;
+import dev.matheus.miniagendamento.core.enums.StatusAgendamento;
+import dev.matheus.miniagendamento.infrastructure.dto.AgendamentoCreateRequest;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class AgendamentoCreateMapper {
+
+
+    public AgendamentoCreateRequest toDTO(Agendamento agendamento){
+        return new AgendamentoCreateRequest(
+                agendamento.titulo(),
+                agendamento.descricao(),
+                agendamento.dataInicio(),
+                agendamento.dataFim(),
+                agendamento.usuario()
+        );
+    }
+
+    public Agendamento toEntity(AgendamentoCreateRequest agendamentoCreateRequest){
+        return new Agendamento(
+                null,
+                agendamentoCreateRequest.titulo(),
+                agendamentoCreateRequest.descricao(),
+                agendamentoCreateRequest.dataInicio(),
+                agendamentoCreateRequest.dataFim(),
+                StatusAgendamento.AGENDADO,
+                agendamentoCreateRequest.usuario(),
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+    }
+
+
+
+}
