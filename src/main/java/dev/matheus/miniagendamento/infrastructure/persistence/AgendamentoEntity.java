11a22b743443dca5581cab88631bc79e9e45c7f2
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-19 17:36:14 -0300

    feat: cria a entidade AgendamentoEntity

---
+++ src/main/java/dev/matheus/miniagendamento/infrastructure/persistence/AgendamentoEntity.java
@@ -0,0 +1,43 @@
+package dev.matheus.miniagendamento.infrastructure.persistence;
+
+import dev.matheus.miniagendamento.core.enums.StatusAgendamento;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "tb_agendamento")
+public class AgendamentoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120)
+    private String titulo;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    @Column(name = "data_inicio", nullable = false)
+    private LocalDateTime dataInicio;
+
+    @Column(name = "data_fim", nullable = false)
+    private LocalDateTime dataFim;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 80)
+    private StatusAgendamento status;
+
+    @Column(name = "criado_em", nullable = false)
+    private LocalDateTime criadoEm;
+
+    @Column(name = "atualizado_em", nullable = false)
+    private LocalDateTime atualizadoEm;
+}
