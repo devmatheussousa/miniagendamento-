889043c7f09a8663b58ed8c9a60545f5aa777d34
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-19 15:58:48 -0300

    feat: implementando BuscarAgendamentoPorIdUseCaseImpl

--- src/main/java/dev/matheus/miniagendamento/core/usecases/BuscarAgendamentoPorIdUseCaseImpl.java
+++ src/main/java/dev/matheus/miniagendamento/core/usecases/BuscarAgendamentoPorIdUseCaseImpl.java
@@ -1,4 +1,22 @@
 package dev.matheus.miniagendamento.core.usecases;
 
-public class BuscarAgendamentoPorIdUseCaseImpl {
+import dev.matheus.miniagendamento.core.entities.Agendamento;
+import dev.matheus.miniagendamento.core.gateway.AgendamentoGateway;
+
+public class BuscarAgendamentoPorIdUseCaseImpl implements BuscarAgendamentoPorIdUseCase {
+
+    private final AgendamentoGateway agendamentoGateway;
+
+    public BuscarAgendamentoPorIdUseCaseImpl(AgendamentoGateway agendamentoGateway) {
+        this.agendamentoGateway = agendamentoGateway;
+    }
+
+    @Override
+    public Agendamento execute(Long id) {
+        var agendamento = agendamentoGateway.buscarAgendamentoPorId(id);
+        if(agendamento == null){
+            throw new IllegalArgumentException("Agendamento não encontrado");
+        }
+        return agendamento;
+    }
 }
