2176c6bb87a036ea9b5f5817cfe1c837268442ae
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-19 19:28:17 -0300

    feat: cria o merge do agendamento

---
+++ src/main/java/dev/matheus/miniagendamento/infrastructure/mapper/AgendamentoUpdateRequestMapper.java
@@ -0,0 +1,42 @@
+package dev.matheus.miniagendamento.infrastructure.mapper;
+
+import dev.matheus.miniagendamento.core.entities.Agendamento;
+import dev.matheus.miniagendamento.infrastructure.dto.AgendamentoUpdateRequest;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class AgendamentoUpdateRequestMapper {
+
+
+    public AgendamentoUpdateRequest toDto(Agendamento agendamento){
+        return new AgendamentoUpdateRequest(
+                agendamento.titulo(),
+                agendamento.descricao(),
+                agendamento.dataInicio(),
+                agendamento.dataFim()
+        );
+    }
+
+    public Agendamento merge(Agendamento agendamentoExistente, AgendamentoUpdateRequest req){
+        return new Agendamento(
+                agendamentoExistente.id(),
+                req.titulo() != null ? req.titulo() : agendamentoExistente.titulo(),
+                req.descricao() != null ? req.descricao() : agendamentoExistente.descricao(),
+                req.dataInicio() != null ? req.dataInicio() : agendamentoExistente.dataInicio(),
+                req.dataFim() != null ? req.dataFim() : agendamentoExistente.dataFim(),
+                agendamentoExistente.status(),
+                agendamentoExistente.usuario(),
+                agendamentoExistente.criadoEm(),
+                LocalDateTime.now()
+        );
+    }
+
+
+
+
+
+
+
+}
