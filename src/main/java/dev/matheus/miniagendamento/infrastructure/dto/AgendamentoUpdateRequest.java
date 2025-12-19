58f208e4f01ff812a62706d6ff1df64546d37a18
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-19 17:58:15 -0300

    feat: criando os DTOs

---
+++ src/main/java/dev/matheus/miniagendamento/infrastructure/dto/AgendamentoUpdateRequest.java
@@ -0,0 +1,16 @@
+package dev.matheus.miniagendamento.infrastructure.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+public record AgendamentoUpdateRequest(
+        @Size(max = 120)
+        String titulo,
+        @Size(max = 4000)
+        String descricao,
+        LocalDateTime dataInicio,
+        LocalDateTime dataFim
+) {
+}
