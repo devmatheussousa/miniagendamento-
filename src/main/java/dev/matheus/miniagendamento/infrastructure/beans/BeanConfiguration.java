4cfe636ce247d41744f02926a51df1b42ec9d7fb
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-20 14:32:14 -0300

    feat: cria o Bens de agendamento

--- src/main/java/dev/matheus/miniagendamento/infrastructure/beans/BeanConfiguration.java
+++ src/main/java/dev/matheus/miniagendamento/infrastructure/beans/BeanConfiguration.java
@@ -1,4 +1,37 @@
 package dev.matheus.miniagendamento.infrastructure.beans;
 
+import dev.matheus.miniagendamento.core.gateway.AgendamentoGateway;
+import dev.matheus.miniagendamento.core.usecases.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BeanConfiguration {
+
+    @Bean
+    public CriarAgendamentoUseCase criarAgendamentoUseCase(AgendamentoGateway agendamentoGateway) {
+        return new CriarAgendamentoUseCaseImpl(agendamentoGateway);
+    }
+
+     @Bean
+    public BuscarAgendamentoPorIdUseCase buscarAgendamentoPorIdUseCase(AgendamentoGateway agendamentoGateway) {
+        return new BuscarAgendamentoPorIdUseCaseImpl(agendamentoGateway);
+    }
+
+    @Bean
+    public AtualizarAgendamentoUseCase atualizarAgendamentoUseCase(AgendamentoGateway agendamentoGateway) {
+        return new AtualizarAgendamentoUserCaseImpl(agendamentoGateway);
+    }
+
+    @Bean
+    public CancelarAgendamentoUseCase cancelarAgendamentoUseCase(AgendamentoGateway agendamentoGateway) {
+        return new CancelarAgendamentoUseCaseImpl(agendamentoGateway);
+    }
+
+    @Bean
+    public ConcluirAgendamentoUseCase concluirAgendamentoUseCase(AgendamentoGateway agendamentoGateway) {
+        return new ConcluirAgendamentoUseCaseimpl(agendamentoGateway);
+    }
+
+
 }
